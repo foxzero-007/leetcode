4f2dce18883a8ca3593bcbb2dc80d8b9400f2ddb
foxzero-007 <2549603631@qq.com>
2021-12-17 09:14:46 +0800

    fix: 21 best update: 26 daily 2021-12-7

--- editor/cn/[21]合并两个有序链表.js
+++ editor/cn/[21]合并两个有序链表.js
@@ -48,33 +48,16 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if(!list1){
+    if(list1===null){
         return list2
-    }
-    if(!list2){
+    }else if(list2===null){
+        return list1
+    }else if(list1.val>list2.val){
+        list2.next=mergeTwoLists(list1,list2.next)
+        return list2
+    }else{
+        list1.next=mergeTwoLists(list1.next,list2)
         return list1
     }
-    if(list1.val<list2.val){
-        var temp = list2
-        list2=list1
-        list1=temp
-    }
-    var res=list2
-    while(list1){
-        if(list2.next&&list2.val<=list1.val&&list1.val<=list2.next.val){
-            var temp = list1.next
-            list1.next=list2.next
-            list2.next=list1
-            list2=list2.next
-            list1=temp
-            console.log(res)
-        }else if(list2.next){
-            list2=list2.next
-        }else{
-            list2.next=list1
-            break
-        }
-    }
-    return res
 };
 //leetcode submit region end(Prohibit modification and deletion)
